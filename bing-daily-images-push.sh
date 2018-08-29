857f806deb24ddd62dd2bbaa04eb38ab53423132
Luis Miguel Vicente Fuentes <kijart@gmail.com>
2018-08-29 19:35:39 +0200

    Split scripts into build and after success

---
+++ bing-daily-images-push.sh
@@ -0,0 +1,12 @@
+function git_commit_push_changes {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+
+  git add .
+  git commit --message "Bing daily images (`date +"%Y-%m-%d"`)"
+
+  git remote add origin https://${GITHUB_TOKEN}@github.com/kijart/bing-daily-images-dl.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin master
+}
+
+git_commit_push_changes

9ceacf9a046ec5071119764b824ba0410c1c577f
Luis Miguel Vicente Fuentes <kijart@gmail.com>
2018-08-29 20:07:12 +0200

    Update git branch for wallpapers

--- bing-daily-images-push.sh
+++ bing-daily-images-push.sh
@@ -2,11 +2,13 @@ function git_commit_push_changes {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 
+  git checkout -b wallpapers
+
   git add .
   git commit --message "Bing daily images (`date +"%Y-%m-%d"`)"
 
   git remote add origin-token https://${GITHUB_TOKEN}@github.com/kijart/bing-daily-images-dl.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-token master
+  git push --quiet --set-upstream origin-token wallpapers
 }
 
 git_commit_push_changes

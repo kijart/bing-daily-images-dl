07850deb700db992a0bacdd2ba9a561712fe69fd
Luis Miguel Vicente Fuentes <kijart@gmail.com>
2019-10-31 17:10:39 +0100

    Update user email on travis ci task

--- bing-daily-images-push.sh
+++ bing-daily-images-push.sh
@@ -1,6 +1,6 @@
 git_commit_push_changes() {
   git remote add origin-token https://${GITHUB_TOKEN}@github.com/kijart/bing-daily-images-dl.git > /dev/null 2>&1
-  git config --global user.email "travis@travis-ci.org"
+  git config --global user.email "kijart+ci@gmail.com"
   git config --global user.name "Travis CI"
 
   git checkout -b wallpapers

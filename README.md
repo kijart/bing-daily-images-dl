b8d58c8d19c3457addfad8db329b5926ab840d73
Luis Miguel Vicente Fuentes <kijart@gmail.com>
2019-11-08 20:28:50 +0100

    Add update task to show latest images on README

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-[![Build Status](https://travis-ci.org/kijart/bing-daily-images-dl.svg?branch=wallpapers)](https://travis-ci.org/kijart/bing-daily-images-dl)
-
 # Wallpaper archive of the Bing search engine
 
+[![Build Status](https://travis-ci.org/kijart/bing-daily-images-dl.svg?branch=wallpapers)](https://travis-ci.org/kijart/bing-daily-images-dl)
+
 This repository downloads [bing.com](https://www.bing.com) wallpapers daily and organizes them in folders.
+
+## Latest images
+
+<!-- Wallpapers -->
--- bing-daily-images-push.sh
+++ bing-daily-images-push.sh
@@ -1,3 +1,21 @@
+update_readme_images() {
+  # remove previous images
+  fixed_line_number=$(cat README.md | awk '/<!-- Wallpapers -->/{ print NR; exit }')
+
+  tmp_readme=$(head -n $fixed_line_number README.md)
+  echo "$tmp_readme\n" > README.md
+
+  # add latest images
+  modified_filename_list=$(git status --porcelain | cut -c4-)
+
+  for filename in $modified_filename_list; do
+    if [[ $filename == *.jpg ]]; then
+      echo "### $filename\n" >> README.md
+      echo "![$filename]($filename)\n" >> README.md
+    fi
+  done
+}
+
 git_commit_push_changes() {
   git remote add origin-token https://${GITHUB_TOKEN}@github.com/kijart/bing-daily-images-dl.git > /dev/null 2>&1
   git config --global user.email "kijart+ci@gmail.com"
@@ -9,4 +27,5 @@ git_commit_push_changes() {
   git push --quiet --set-upstream origin-token master
 }
 
+update_readme_images
 git_commit_push_changes

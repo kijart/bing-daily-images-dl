102f2c0411ec181581f79b04e2bac38c69cbd3d8
Luis Miguel Vicente Fuentes <1217588+kijart@users.noreply.github.com>
2021-05-02 19:33:33 +0200

    Update git user email on push script

--- bing-daily-images-push.sh
+++ bing-daily-images-push.sh
@@ -18,7 +18,7 @@ update_readme_images() {
 
 git_remote_setup() {
   git remote add origin-token https://"${GITHUB_TOKEN}"@github.com/kijart/bing-daily-images-dl.git > /dev/null 2>&1
-  git config --global user.email "kijart+ci@gmail.com"
+  git config --global user.email "1217588+kijart@users.noreply.github.com"
   git config --global user.name "Travis CI"
 
   git add .
